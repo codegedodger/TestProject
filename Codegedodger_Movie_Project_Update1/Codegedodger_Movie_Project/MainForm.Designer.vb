aa543fe1e0353ba2c21819648b53e69b9f5e897f
codegedodger\HP <codebeginnershere@gmail.com>
2016-11-02 23:20:24 +0500

    dealt with a few controls, set some of the button handlers to work and to display other associated forms followed by the main form.

---
+++ Codegedodger_Movie_Project_Update1/Codegedodger_Movie_Project/MainForm.Designer.vb
@@ -0,0 +1,62 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnAdmin = New System.Windows.Forms.Button()
+        Me.btnUser = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnAdmin
+        '
+        Me.btnAdmin.Location = New System.Drawing.Point(202, 51)
+        Me.btnAdmin.Name = "btnAdmin"
+        Me.btnAdmin.Size = New System.Drawing.Size(136, 23)
+        Me.btnAdmin.TabIndex = 0
+        Me.btnAdmin.Text = "Go As Admin"
+        Me.btnAdmin.UseVisualStyleBackColor = True
+        '
+        'btnUser
+        '
+        Me.btnUser.Location = New System.Drawing.Point(202, 115)
+        Me.btnUser.Name = "btnUser"
+        Me.btnUser.Size = New System.Drawing.Size(136, 23)
+        Me.btnUser.TabIndex = 1
+        Me.btnUser.Text = "Go As User"
+        Me.btnUser.UseVisualStyleBackColor = True
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(565, 210)
+        Me.Controls.Add(Me.btnUser)
+        Me.Controls.Add(Me.btnAdmin)
+        Me.Name = "MainForm"
+        Me.Text = "MainForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnAdmin As Button
+    Friend WithEvents btnUser As Button
+End Class
