2d5bb360750402ddaf937e1b350b31a4f8859d17
codegedodger\HP <codebeginnershere@gmail.com>
2016-11-05 23:21:44 +0500

    Created Dataset and added its gridview to the forms and make it work. Now tables from database appears exacly on the forms.

---
+++ Codegedodger_Movie_Project_Updated3/Codegedodger_Movie_Project/Movie_CRUD.vb
@@ -0,0 +1,14 @@
+﻿Public Class Movie_CRUD
+    Private Sub MoviesBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) 
+        Me.Validate()
+        Me.MoviesBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.MoviesdbDataSet)
+
+    End Sub
+
+    Private Sub Movie_CRUD_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'MoviesdbDataSet.Movies' table. You can move, or remove it, as needed.
+        Me.MoviesTableAdapter.Fill(Me.MoviesdbDataSet.Movies)
+
+    End Sub
+End Class
