fcecbeeb27d3ad18ea2841929f2423354eff1e67
codegedodger\HP <codebeginnershere@gmail.com>
2016-11-09 22:06:47 +0500

    Little change of plan, I decided to do first thing first, so I did logins today for both user and admin and successfuly ran queries with database, also created tables in database, one more thing that I intend to do is user registeration. user can register and will add values which will directly be saved in database.

---
+++ Codegedodger_Movie_Project_Update4/Codegedodger_Movie_Project/Admin_login.vb
@@ -0,0 +1,31 @@
+﻿
+
+Imports Codegedodger_Movie_Project.MoviesdbDataSet
+Imports Codegedodger_Movie_Project.MoviesdbDataSetTableAdapters
+Imports System.Data.SqlClient
+
+
+Public Class Admin_login
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+
+        Dim con As New SqlConnection
+        Dim cmd As New SqlCommand
+        Dim rd As SqlDataReader
+        con.ConnectionString = "Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\HP\documents\visual studio 2015\Projects\Codegedodger_Movie_Project\Codegedodger_Movie_Project\Moviesdb.mdf;Integrated Security=True"
+        cmd.Connection = con
+        con.Open()
+        cmd.CommandText = "select A_username,A_password from Admin where A_username= '" & txtUName.Text & "' and A_password='" & btnPassword.Text & "' "
+        rd = cmd.ExecuteReader
+        If rd.HasRows Then
+            Movie_CRUD.Show()
+        Else
+            MessageBox.Show("Invalid username or password")
+
+        End If
+    End Sub
+End Class
---
+++ Codegedodger_Movie_Project_Update4/Codegedodger_Movie_Project/UserForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class UserForm
+    Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
+        User_Login.Show()
+    End Sub
+
+    Private Sub btnRegisteration_Click(sender As Object, e As EventArgs) Handles btnRegisteration.Click
+        User_Registration.Show()
+    End Sub
+End Class
